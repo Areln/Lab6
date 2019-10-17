2285440c9353fe3932d1748d051e1195d4a5774e
Aaron <devilfruits1999@gmail.com>
2019-10-17 15:55:32 -0400

    First Commit

---
+++ Lab6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string runAgain = "y";
+            int userInput = 0;
+            while (runAgain != "n")
+            {
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.Write("Please Enter a integer: ");
+                while (!int.TryParse(Console.ReadLine(), out userInput))
+                {
+                    Console.Write("Enter an integer: ");
+                }
+                Console.WriteLine("Number\tSquared\tCubed");
+                Console.WriteLine("======\t======\t======");
+                for (int i = 1; i < userInput + 1; i++)
+                {
+                    Console.WriteLine($"{i}\t{i * i}\t{i * i * i}");
+                }
+                Console.WriteLine("Run again? (y/n)");
+                runAgain = Console.ReadLine();
+            }
+        }
+    }
+}
